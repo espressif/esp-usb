94cf0902bbc9a12c757c9e59ffcc7878a24c8e91
Li Bo <libo@espressif.com>
2024-03-22 21:58:41 +0800

    feat: add usb host uac driver

---
+++ host/class/uac/usb_host_uac/examples/usb_audio_player/main/usb_audio_player_main.c
@@ -0,0 +1,326 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_spiffs.h"
+#include "usb/usb_host.h"
+#include "usb/uac_host.h"
+#include "audio_player.h"
+
+static const char *TAG = "usb_audio_player";
+
+#define USB_HOST_TASK_PRIORITY  5
+#define UAC_TASK_PRIORITY       5
+#define USER_TASK_PRIORITY      2
+#define SPIFFS_BASE             "/spiffs"
+#define MP3_FILE_NAME           "/new_epic.mp3"
+#define BIT1_SPK_START          (0x01 << 0)
+
+static EventGroupHandle_t s_evt_handle;
+static QueueHandle_t s_event_queue = NULL;
+static uac_host_device_handle_t s_spk_dev_handle = NULL;
+static FILE *s_fp = NULL;
+static void uac_device_callback(uac_host_device_handle_t uac_device_handle, const uac_host_device_event_t event, void *arg);
+/**
+ * @brief event group
+ *
+ * APP_EVENT            - General control event
+ * UAC_DRIVER_EVENT     - UAC Host Driver event, such as device connection
+ * UAC_DEVICE_EVENT     - UAC Host Device event, such as rx/tx completion, device disconnection
+ */
+typedef enum {
+    APP_EVENT = 0,
+    UAC_DRIVER_EVENT,
+    UAC_DEVICE_EVENT,
+} event_group_t;
+
+/**
+ * @brief event queue
+ *
+ * This event is used for delivering the UAC Host event from callback to the uac_lib_task
+ */
+typedef struct {
+    event_group_t event_group;
+    union {
+        struct {
+            uint8_t addr;
+            uint8_t iface_num;
+            uac_host_driver_event_t event;
+            void *arg;
+        } driver_evt;
+        struct {
+            uac_host_device_handle_t handle;
+            uac_host_driver_event_t event;
+            void *arg;
+        } device_evt;
+    };
+} s_event_queue_t;
+
+static esp_err_t _audio_player_mute_fn(AUDIO_PLAYER_MUTE_SETTING setting)
+{
+    if (s_spk_dev_handle == NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+    ESP_LOGI(TAG, "mute setting: %s", setting == 0 ? "mute" : "unmute");
+
+    return uac_host_device_control(s_spk_dev_handle, UAC_CTRL_UAC_MUTE, (void *)(setting == 0 ? 1 : 0));
+}
+
+static esp_err_t _audio_player_write_fn(void *audio_buffer, size_t len, size_t *bytes_written, uint32_t timeout_ms)
+{
+    if (s_spk_dev_handle == NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+    *bytes_written = 0;
+    esp_err_t ret = uac_host_device_write(s_spk_dev_handle, audio_buffer, len, timeout_ms);
+    if (ret == ESP_OK) {
+        *bytes_written = len;
+    }
+    return ret;
+}
+
+static esp_err_t _audio_player_std_clock(uint32_t rate, uint32_t bits_cfg, i2s_slot_mode_t ch)
+{
+    ESP_LOGI(TAG, "Re-config: speaker rate %"PRIu32", bits %"PRIu32", mode %s", rate, bits_cfg, ch == 1 ? "MONO" : (ch == 2 ? "STEREO" : "INVALID"));
+    ESP_ERROR_CHECK(uac_host_device_stop(s_spk_dev_handle));
+    const uac_host_stream_config_t stm_config = {
+        .channels = ch,
+        .bit_resolution = bits_cfg,
+        .sample_freq = rate,
+    };
+    return uac_host_device_start(s_spk_dev_handle, &stm_config);
+}
+
+static void _audio_player_callback(audio_player_cb_ctx_t *ctx)
+{
+    ESP_LOGI(TAG, "ctx->audio_event = %d", ctx->audio_event);
+    switch (ctx->audio_event) {
+    case AUDIO_PLAYER_CALLBACK_EVENT_IDLE: {
+        ESP_LOGI(TAG, "AUDIO_PLAYER_REQUEST_IDLE");
+        ESP_ERROR_CHECK(uac_host_device_suspend(s_spk_dev_handle));
+        ESP_LOGI(TAG, "Play in loop");
+        s_fp = fopen(SPIFFS_BASE MP3_FILE_NAME, "rb");
+        if (s_fp) {
+            ESP_LOGI(TAG, "Playing '%s'", MP3_FILE_NAME);
+            audio_player_play(s_fp);
+        } else {
+            ESP_LOGE(TAG, "unable to open filename '%s'", MP3_FILE_NAME);
+        }
+        break;
+    }
+    case AUDIO_PLAYER_CALLBACK_EVENT_PLAYING:
+        ESP_ERROR_CHECK(uac_host_device_resume(s_spk_dev_handle));
+        ESP_ERROR_CHECK(uac_host_device_control(s_spk_dev_handle, UAC_CTRL_UAC_VOLUME, (void *)60));
+        ESP_LOGI(TAG, "AUDIO_PLAYER_REQUEST_PLAY");
+        break;
+    case AUDIO_PLAYER_CALLBACK_EVENT_PAUSE:
+        ESP_ERROR_CHECK(uac_host_device_suspend(s_spk_dev_handle));
+        ESP_LOGI(TAG, "AUDIO_PLAYER_REQUEST_PAUSE");
+        break;
+    default:
+        break;
+    }
+}
+
+static void uac_device_callback(uac_host_device_handle_t uac_device_handle, const uac_host_device_event_t event, void *arg)
+{
+    if (event == UAC_HOST_DRIVER_EVENT_DISCONNECTED) {
+        ESP_LOGI(TAG, "UAC Device disconnected");
+        ESP_ERROR_CHECK(uac_host_device_close(uac_device_handle));
+        return;
+    }
+    // Send uac device event to the event queue
+    s_event_queue_t evt_queue = {
+        .event_group = UAC_DEVICE_EVENT,
+        .device_evt.handle = uac_device_handle,
+        .device_evt.event = event,
+        .device_evt.arg = arg
+    };
+    // should not block here
+    xQueueSend(s_event_queue, &evt_queue, 0);
+}
+
+static void uac_host_lib_callback(uint8_t addr, uint8_t iface_num, const uac_host_driver_event_t event, void *arg)
+{
+    // Send uac driver event to the event queue
+    s_event_queue_t evt_queue = {
+        .event_group = UAC_DRIVER_EVENT,
+        .driver_evt.addr = addr,
+        .driver_evt.iface_num = iface_num,
+        .driver_evt.event = event,
+        .driver_evt.arg = arg
+    };
+    xQueueSend(s_event_queue, &evt_queue, 0);
+}
+
+/**
+ * @brief Start USB Host install and handle common USB host library events while app pin not low
+ *
+ * @param[in] arg  Not used
+ */
+static void usb_lib_task(void *arg)
+{
+    const usb_host_config_t host_config = {
+        .skip_phy_setup = false,
+        .intr_flags = ESP_INTR_FLAG_LEVEL1,
+    };
+
+    ESP_ERROR_CHECK(usb_host_install(&host_config));
+    ESP_LOGI(TAG, "USB Host installed");
+    xTaskNotifyGive(arg);
+
+    while (true) {
+        uint32_t event_flags;
+        usb_host_lib_handle_events(portMAX_DELAY, &event_flags);
+        // In this example, there is only one client registered
+        // So, once we deregister the client, this call must succeed with ESP_OK
+        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_NO_CLIENTS) {
+            ESP_ERROR_CHECK(usb_host_device_free_all());
+            break;
+        }
+    }
+
+    ESP_LOGI(TAG, "USB Host shutdown");
+    // Clean up USB Host
+    vTaskDelay(10); // Short delay to allow clients clean-up
+    ESP_ERROR_CHECK(usb_host_uninstall());
+    vTaskDelete(NULL);
+}
+
+static void uac_lib_task(void *arg)
+{
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    uac_host_driver_config_t uac_config = {
+        .create_background_task = true,
+        .task_priority = UAC_TASK_PRIORITY,
+        .stack_size = 4096,
+        .core_id = 0,
+        .callback = uac_host_lib_callback,
+        .callback_arg = NULL
+    };
+
+    ESP_ERROR_CHECK(uac_host_install(&uac_config));
+    ESP_LOGI(TAG, "UAC Class Driver installed");
+    s_event_queue_t evt_queue = {0};
+    while (1) {
+        if (xQueueReceive(s_event_queue, &evt_queue, portMAX_DELAY)) {
+            if (UAC_DRIVER_EVENT ==  evt_queue.event_group) {
+                uac_host_driver_event_t event = evt_queue.driver_evt.event;
+                uint8_t addr = evt_queue.driver_evt.addr;
+                uint8_t iface_num = evt_queue.driver_evt.iface_num;
+                switch (event) {
+                case UAC_HOST_DRIVER_EVENT_CONNECTED: {
+                    uac_host_dev_info_t dev_info;
+                    uac_host_device_handle_t uac_device_handle = NULL;
+                    const uac_host_device_config_t dev_config = {
+                        .addr = addr,
+                        .iface_num = iface_num,
+                        .buffer_size = 16000,
+                        .buffer_threshold = 4000,
+                        .callback = uac_device_callback,
+                        .callback_arg = NULL,
+                    };
+                    ESP_ERROR_CHECK(uac_host_device_open(&dev_config, &uac_device_handle));
+                    ESP_ERROR_CHECK(uac_host_get_device_info(uac_device_handle, &dev_info));
+                    if (dev_info.type == UAC_STREAM_RX) {
+                        ESP_LOGI(TAG, "UAC Device connected: MIC");
+                        // we don't MIC, just close it
+                        ESP_ERROR_CHECK(uac_host_device_close(uac_device_handle));
+                        continue;
+                    } else if (dev_info.type == UAC_STREAM_TX) {
+                        ESP_LOGI(TAG, "UAC Device connected: SPK");
+                        s_spk_dev_handle = uac_device_handle;
+                    }
+                    uac_host_printf_device_param(uac_device_handle);
+                    const uac_host_stream_config_t stm_config = {
+                        .channels = 2,
+                        .bit_resolution = 16,
+                        .sample_freq = 48000,
+                    };
+                    ESP_ERROR_CHECK(uac_host_device_start(s_spk_dev_handle, &stm_config));
+                    s_fp = fopen(SPIFFS_BASE MP3_FILE_NAME, "rb");
+                    if (s_fp) {
+                        ESP_LOGI(TAG, "Playing '%s'", MP3_FILE_NAME);
+                        audio_player_play(s_fp);
+                    } else {
+                        ESP_LOGE(TAG, "unable to open filename '%s'", MP3_FILE_NAME);
+                    }
+                    break;
+                }
+                default:
+                    break;
+                }
+            } else if (UAC_DEVICE_EVENT == evt_queue.event_group) {
+                uac_host_device_event_t event = evt_queue.device_evt.event;
+                switch (event) {
+                case UAC_HOST_DRIVER_EVENT_DISCONNECTED:
+                    ESP_LOGI(TAG, "UAC Device disconnected");
+                    if (s_fp) {
+                        audio_player_stop();
+                        s_fp = NULL;
+                    }
+                    break;
+                case UAC_HOST_DEVICE_EVENT_RX_DONE:
+                    break;
+                case UAC_HOST_DEVICE_EVENT_TX_DONE:
+                    break;
+                case UAC_HOST_DEVICE_EVENT_TRANSFER_ERROR:
+                    break;
+                default:
+                    break;
+                }
+            } else if (APP_EVENT == evt_queue.event_group) {
+                break;
+            }
+        }
+    }
+
+    ESP_LOGI(TAG, "UAC Driver uninstall");
+    ESP_ERROR_CHECK(uac_host_uninstall());
+}
+
+void app_main(void)
+{
+    esp_log_level_set("*", ESP_LOG_INFO);
+    s_evt_handle = xEventGroupCreate();
+    assert(s_evt_handle != NULL);
+    s_event_queue = xQueueCreate(10, sizeof(s_event_queue_t));
+    assert(s_event_queue != NULL);
+
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = SPIFFS_BASE,
+        .partition_label = NULL,
+        .max_files = 2,
+        .format_if_mount_failed = true,
+    };
+    ESP_ERROR_CHECK(esp_vfs_spiffs_register(&conf));
+    audio_player_config_t config = {.mute_fn = _audio_player_mute_fn,
+                                    .write_fn = _audio_player_write_fn,
+                                    .clk_set_fn = _audio_player_std_clock,
+                                    .priority = 1
+                                   };
+    ESP_ERROR_CHECK(audio_player_new(config));
+    ESP_ERROR_CHECK(audio_player_callback_register(_audio_player_callback, NULL));
+
+    static TaskHandle_t uac_task_handle = NULL;
+    BaseType_t ret = xTaskCreatePinnedToCore(uac_lib_task, "uac_events", 4096, NULL,
+                     USER_TASK_PRIORITY, &uac_task_handle, 0);
+    assert(ret == pdTRUE);
+    ret = xTaskCreatePinnedToCore(usb_lib_task, "usb_events", 4096, (void *)uac_task_handle,
+                                  USB_HOST_TASK_PRIORITY, NULL, 0);
+    assert(ret == pdTRUE);
+
+    while (1) {
+        vTaskDelay(100);
+    }
+
+}
