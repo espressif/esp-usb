d55ffc0ee4faa8d2d52d86b936cc95644896da10
leeebo <libo_go@163.com>
2024-05-08 08:03:08 +0800

    Update host/class/uac/usb_host_uac/test_app/pytest_usb_host_uac.py
    Co-authored-by: Peter Marčišovský <109745740+peter-marcisovsky@users.noreply.github.com>

--- host/class/uac/usb_host_uac/test_app/pytest_usb_host_uac.py
+++ host/class/uac/usb_host_uac/test_app/pytest_usb_host_uac.py
@@ -6,11 +6,9 @@ from typing import Tuple
 import pytest
 from pytest_embedded_idf.dut import IdfDut
 
-
+# No runner marker, unable to mock UAC 1.0 device with tinyusb
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
-@pytest.mark.temp_skip_ci(targets=['esp32s2', 'esp32s3'], reason='unable to mock UAC 1.0 device with tinyusb')
-@pytest.mark.usb_host
 def test_usb_host_uac(dut: IdfDut) -> None:
     dut.expect_exact('Press ENTER to see the list of tests.')
     dut.write('[uac_host]')
