0a2c7fb29519b36eea176aeb359e09e3b3d6dd53
Roman Leonov <roman.leonov@espressif.com>
2025-01-02 12:00:25 +0100

    refactor(cdc_and_usb_device): Splitted the tests to cdc and to dconn_detection

--- device/esp_tinyusb/test_apps/dconn_detection/pytest_dconn_detection.py
+++ device/esp_tinyusb/test_apps/dconn_detection/pytest_dconn_detection.py
@@ -9,5 +9,5 @@ from pytest_embedded_idf.dut import IdfDut
 @pytest.mark.esp32s3
 @pytest.mark.esp32p4
 #@pytest.mark.usb_device            Disable in CI: missing tinyusb teardown
-def test_usb_device_esp_tinyusb(dut: IdfDut) -> None:
+def test_usb_device_dconn_detection(dut: IdfDut) -> None:
     dut.run_all_single_board_cases(group='usb_device')
