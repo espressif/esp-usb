78597cbd9d16cf523161ad92f463e1e4964a4152
Roman Leonov <roman.leonov@espressif.com>
2025-01-15 20:45:55 +0100

    change(ci): Marked off the p4 test

--- device/esp_tinyusb/test_apps/dconn_detection/pytest_dconn_detection.py
+++ device/esp_tinyusb/test_apps/dconn_detection/pytest_dconn_detection.py
@@ -7,6 +7,8 @@ from pytest_embedded_idf.dut import IdfDut
 
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
+@pytest.mark.esp32p4
+@pytest.mark.temp_skip_ci(targets=['esp32p4'], reason='P4 conn/dconn emulation has not been implmented')
 @pytest.mark.usb_device
 def test_usb_device_dconn_detection(dut: IdfDut) -> None:
     dut.run_all_single_board_cases(group='dconn')
