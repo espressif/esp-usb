77268dd2907688c35c8634441abe82441a8bd5e3
Roman Leonov <roman.leonov@espressif.com>
2025-01-20 12:39:28 +0100

    refactor(esp_tinyusb): Added run-time configuration parameters

--- device/esp_tinyusb/include/tinyusb_types.h
+++ device/esp_tinyusb/include/tinyusb_types.h
@@ -1,10 +1,13 @@
 /*
- * SPDX-FileCopyrightText: 2020-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2020-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
+
+#include "soc/soc_caps.h"
+#include "sdkconfig.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,6 +18,28 @@ extern "C" {
 typedef enum {
     TINYUSB_USBDEV_0,
 } tinyusb_usbdev_t;
+
+/**
+ * @brief TinyUSB peripheral port number
+ */
+typedef enum {
+    TINYUSB_PORT_0 = 0,                /*!< USB OTG 1.1 peripheral */
+#if (SOC_USB_OTG_PERIPH_NUM > 1)
+    TINYUSB_PORT_1,                    /*!< USB OTG 2.0 peripheral */
+#endif // (SOC_USB_OTG_PERIPH_NUM > 1)
+    TINYUSB_PORT_MAX,
+} tinyusb_port_t;
+
+/**
+ * @brief TinyUSB Task affinity mask
+ */
+typedef enum {
+    TINYUSB_TASK_AFFINITY_CPU0,
+#if !CONFIG_FREERTOS_UNICORE
+    TINYUSB_TASK_AFFINITY_CPU1,
+#endif // !CONFIG_FREERTOS_UNICORE
+    TINYUSB_TASK_AFFINITY_MAX
+} tinyusb_task_affinity_mask_t;
 
 #ifdef __cplusplus
 }
