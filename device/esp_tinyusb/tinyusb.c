6a815c55c3d0380a85243aef8f60fa39f7ac9b59
Roman Leonov <roman.leonov@espressif.com>
2025-04-25 12:52:41 +0200

    refactor(tinyusb): Descriptors config

--- device/esp_tinyusb/tinyusb.c
+++ device/esp_tinyusb/tinyusb.c
@@ -8,7 +8,7 @@
 #include "esp_log.h"
 #include "esp_check.h"
 #include "esp_err.h"
-#include "esp_private/periph_ctrl.h"
+// #include "esp_private/periph_ctrl.h"
 #include "esp_private/usb_phy.h"
 #include "tinyusb.h"
 #include "descriptors_control.h"
@@ -18,7 +18,7 @@ const static char *TAG = "TinyUSB";
 
 // Tinyusb supports only one instance of the driver per peripheral
 // Otherwise, the next parameters could be stored in the context
-static usb_phy_handle_t phy_hdl;
+static usb_phy_handle_t s_phy_hdl;
 
 // For the tinyusb component without tusb_teardown() implementation
 #ifndef tusb_teardown
@@ -43,40 +43,39 @@ static esp_err_t tinyusb_check_config(const tinyusb_config_t *config)
 esp_err_t tinyusb_driver_install(const tinyusb_config_t *config)
 {
     ESP_RETURN_ON_ERROR(tinyusb_check_config(config), TAG, "TinyUSB configuration check failed");
+    ESP_RETURN_ON_ERROR(tinyusb_descriptors_check(config->port, &config->descriptor), TAG, "TinyUSB descriptors check failed");
     ESP_RETURN_ON_ERROR(tinyusb_task_check_config(&config->task), TAG, "TinyUSB task configuration check failed");
 
     esp_err_t ret;
-
-    if (!config->skip_phy_setup) {
+    usb_phy_handle_t phy_hdl = NULL;
+    if (!config->phy.skip_setup) {
         // Configure USB PHY
         usb_phy_config_t phy_conf = {
             .controller = USB_PHY_CTRL_OTG,
             .target = USB_PHY_TARGET_INT,
             .otg_mode = USB_OTG_MODE_DEVICE,
             // OTG speed selects the PHY indirectly
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 3, 0)
+            // esp32p4 is supported from IDF 5.3
             .otg_speed = (config->port == TINYUSB_PORT_0) ? USB_PHY_SPEED_FULL : USB_PHY_SPEED_HIGH,
+#endif // ESP_IDF >= 5.3
         };
 
         // OTG IOs config
-        const usb_phy_otg_io_conf_t otg_io_conf = USB_PHY_SELF_POWERED_DEVICE(config->vbus_monitor_io);
-        if (config->self_powered) {
+        const usb_phy_otg_io_conf_t otg_io_conf = USB_PHY_SELF_POWERED_DEVICE(config->phy.vbus_monitor_io);
+        if (config->phy.self_powered) {
             phy_conf.otg_io_conf = &otg_io_conf;
         }
         ESP_RETURN_ON_ERROR(usb_new_phy(&phy_conf, &phy_hdl), TAG, "Install USB PHY failed");
     }
-
-    // Descriptors config
-    ESP_GOTO_ON_ERROR(tinyusb_set_descriptors(config), del_phy, TAG, "Descriptors config failed");
     // Init TinyUSB stack in task
-    ESP_GOTO_ON_ERROR(tinyusb_task_start(config->port, &config->task), free_desc, TAG, "Init TinyUSB task failed");
-
+    ESP_GOTO_ON_ERROR(tinyusb_task_start(config->port, &config->task, &config->descriptor), del_phy, TAG, "Init TinyUSB task failed");
+    s_phy_hdl = phy_hdl; // Save the PHY handle for uninstallation
     ESP_LOGI(TAG, "TinyUSB Driver installed on port %d", config->port);
     return ESP_OK;
 
-free_desc:
-    tinyusb_free_descriptors();
 del_phy:
-    if (!config->skip_phy_setup) {
+    if (!config->phy.skip_setup) {
         usb_del_phy(phy_hdl);
     }
     return ret;
@@ -85,7 +84,8 @@ del_phy:
 esp_err_t tinyusb_driver_uninstall(void)
 {
     ESP_RETURN_ON_ERROR(tinyusb_task_stop(), TAG, "Deinit TinyUSB task failed");
-    tinyusb_free_descriptors();
-    ESP_RETURN_ON_ERROR(usb_del_phy(phy_hdl), TAG, "Unable to delete PHY");
+    if (s_phy_hdl) {
+        ESP_RETURN_ON_ERROR(usb_del_phy(s_phy_hdl), TAG, "Unable to delete PHY");
+    }
     return ESP_OK;
 }
