94031aba3b53f4d9b8cbdb0a1ad52c1c275a86bf
Roman Leonov <roman.leonov@espressif.com>
2025-05-22 12:12:35 +0200

    feature(tinyusb): Added tinyusb user event callback

--- device/esp_tinyusb/tinyusb.c
+++ device/esp_tinyusb/tinyusb.c
@@ -13,16 +13,84 @@
 #include "tinyusb_task.h"
 #include "tusb.h"
 
+#if (CONFIG_TINYUSB_MSC_ENABLED)
+#include "tusb_msc_storage.h"
+#endif // CONFIG_TINYUSB_MSC_ENABLED
+
 const static char *TAG = "TinyUSB";
 
-// Tinyusb supports only one instance of the driver per peripheral
-// Otherwise, the next parameters could be stored in the context
-static usb_phy_handle_t s_phy_hdl;
+/**
+ * @brief TinyUSB context
+ */
+typedef struct {
+    tinyusb_port_t port;                      /*!< USB Peripheral hardware port number. Available when hardware has several available peripherals. */
+    usb_phy_handle_t phy_hdl;                 /*!< USB PHY handle */
+    tinyusb_event_cb_t event_cb;              /*!< Callback function that will be called when USB events occur. */
+    void *event_arg;                          /*!< Pointer to the argument passed to the callback */
+} tinyusb_ctx_t;
+
+static tinyusb_ctx_t s_ctx; // TinyUSB context
 
 // For the tinyusb component without tusb_teardown() implementation
 #ifndef tusb_teardown
 #   define tusb_teardown()   (true)
 #endif // tusb_teardown
+
+// ==================================================================================
+// ============================= TinyUSB Callbacks ==================================
+// ==================================================================================
+
+/**
+ * @brief Callback function invoked when device is mounted (configured)
+ *
+ * This function is called by TinyUSB stack when:
+ *
+ * - SetConfiguration(n) is called by the host, where n is the configuration number and not zero.
+ *
+ * @note
+ * For Win-based Hosts: SetConfiguration(n) request is present only with available Class in Device Descriptor.
+ */
+void tud_mount_cb(void)
+{
+#if (CONFIG_TINYUSB_MSC_ENABLED)
+    tinyusb_msc_storage_to_usb();
+#endif // CONFIG_TINYUSB_MSC_ENABLED
+    tinyusb_event_t event = {
+        .id = TINYUSB_EVENT_ATTACHED,
+        .rhport = s_ctx.port,
+    };
+
+    if (s_ctx.event_cb) {
+        s_ctx.event_cb(&event, s_ctx.event_arg);
+    }
+}
+
+/**
+ * @brief Callback function invoked when device is unmounted
+ *
+ * This function is called by TinyUSB stack when:
+ *
+ * - SetConfiguration(0) is called by the host.
+ * - Device is disconnected (DCD_EVENT_UNPLUGGED) from the host.
+ */
+void tud_umount_cb(void)
+{
+#if (CONFIG_TINYUSB_MSC_ENABLED)
+    tinyusb_msc_storage_to_app();
+#endif // CONFIG_TINYUSB_MSC_ENABLED
+    tinyusb_event_t event = {
+        .id = TINYUSB_EVENT_DETACHED,
+        .rhport = s_ctx.port,
+    };
+
+    if (s_ctx.event_cb) {
+        s_ctx.event_cb(&event, s_ctx.event_arg);
+    }
+}
+
+// ==================================================================================
+// ============================= ESP TinyUSB Driver =================================
+// ==================================================================================
 
 /**
  * @brief Check the TinyUSB configuration
@@ -72,7 +140,12 @@ esp_err_t tinyusb_driver_install(const tinyusb_config_t *config)
     }
     // Init TinyUSB stack in task
     ESP_GOTO_ON_ERROR(tinyusb_task_start(config->port, &config->task, &config->descriptor), del_phy, TAG, "Init TinyUSB task failed");
-    s_phy_hdl = phy_hdl; // Save the PHY handle for uninstallation
+
+    s_ctx.port = config->port;              // Save the port number
+    s_ctx.phy_hdl = phy_hdl;                // Save the PHY handle for uninstallation
+    s_ctx.event_cb = config->event_cb;      // Save the event callback
+    s_ctx.event_arg = config->event_arg;    // Save the event callback argument
+
     ESP_LOGI(TAG, "TinyUSB Driver installed on port %d", config->port);
     return ESP_OK;
 
@@ -86,9 +159,9 @@ del_phy:
 esp_err_t tinyusb_driver_uninstall(void)
 {
     ESP_RETURN_ON_ERROR(tinyusb_task_stop(), TAG, "Deinit TinyUSB task failed");
-    if (s_phy_hdl) {
-        ESP_RETURN_ON_ERROR(usb_del_phy(s_phy_hdl), TAG, "Unable to delete PHY");
-        s_phy_hdl = NULL;
+    if (s_ctx.phy_hdl) {
+        ESP_RETURN_ON_ERROR(usb_del_phy(s_ctx.phy_hdl), TAG, "Unable to delete PHY");
+        s_ctx.phy_hdl = NULL;
     }
     return ESP_OK;
 }
