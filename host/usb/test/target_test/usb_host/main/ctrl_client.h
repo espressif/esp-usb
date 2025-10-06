cc63fe25541f51f4d16f2ccaa308052bff4e17cb
peter.marcisovsky <peter.marcisovsky@espressif.com>
2025-10-06 17:22:55 +0200

    feat(usb_host): Remote wakeup
        - HCLK Gating to save power on the host side
        - HCD + USB Host tests

--- host/usb/test/target_test/usb_host/main/ctrl_client.h
+++ host/usb/test/target_test/usb_host/main/ctrl_client.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2021 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -11,3 +11,5 @@ typedef struct {
 } ctrl_client_test_param_t;
 
 void ctrl_client_async_seq_task(void *arg);
+
+void ctrl_client_remote_wake_task(void *arg);
