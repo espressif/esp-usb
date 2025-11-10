9ca7924b3d2188148a9dbd4938d48cf8cff5b580
Roman Leonov <roman.leonov@espressif.com>
2025-11-10 11:31:26 +0100

    refactor(esp_tinyusb): Added workaround for vbus monitor for DWC OTG v4.30a
    - Added the callback to vbus_monitor
    - Applied workaround fix to replace the UNPLUG event with direct notification for USB OTG 2.0 v4.30a

--- device/esp_tinyusb/tinyusb_vbus_monitor.c
+++ device/esp_tinyusb/tinyusb_vbus_monitor.c
@@ -15,11 +15,24 @@
 
 const static char *TAG = "VBUS mon";
 
+// This value is used for checking Timer Task Stack Size when MSC is enabled
+// When value is changed, also update the README.md documentation
+#define VBUS_MON_TIMER_TASK_STACK_MIN    2304
+
 #if (CONFIG_IDF_TARGET_ESP32P4)
-#include "soc/usb_dwc_struct.h"
 // On ESP32-P4 USB OTG 2.0 signals are not wired to GPIO matrix
 // So we need to override the Bvalid signal from PHY
+#include "soc/usb_dwc_struct.h"         // For GOTGCTL (when supported) and DCTL registers access
 #define USB_DWC_REG                     USB_DWC_HS
+
+#if (CONFIG_ESP32P4_REV_MIN_300)
+// On ESP32-P4 ECO5, BVALID override is not supported
+#define BVALID_OVERRIDE_SUPPORT_ENABLE  0
+#else
+#define BVALID_OVERRIDE_SUPPORT_ENABLE  1
+#endif // CONFIG_ESP32P4_REV_MIN_300
+#else
+#error "Software-based VBUS monitoring functionality is intended exclusively for ESP32-P4, USB OTG 2.0"
 #endif // CONFIG_IDF_TARGET_ESP32P4
 
 /**
@@ -31,18 +44,22 @@ typedef struct {
     gpio_num_t gpio_num;            /*!< GPIO number used for VBUS monitoring */
     bool prev_state;                /*!< Previous VBUS IO state: true - HIGH, false - LOW */
     TimerHandle_t debounce_timer;   /*!< Debounce timer handle */
+    vbus_mon_state_changed_t state_changed_cb;  /*!< Callback function called when VBUS state changes */
 } vbus_monitor_context_t;
+
 
 static vbus_monitor_context_t _vbus_ctx = {
     .gpio_num = GPIO_NUM_NC,
     .prev_state = false,
     .debounce_timer = NULL,
+    .state_changed_cb = NULL,
 };
 
 //
 // Additional low-level USB DWC functions, which are not present in the IDF USB DWC HAL
 //
 
+#if (BVALID_OVERRIDE_SUPPORT_ENABLE)
 // --------------- GOTGCTL register ------------------
 
 static void usb_dwc_ll_gotgctl_set_bvalid_override_value(usb_dwc_dev_t *hw, uint8_t value)
@@ -54,6 +71,7 @@ static void usb_dwc_ll_gotgctl_enable_bvalid_override(usb_dwc_dev_t *hw, bool en
 {
     hw->gotgctl_reg.bvalidoven = enable ? 1 : 0;
 }
+#endif // BVALID_OVERRIDE_SUPPORT_ENABLE
 
 // ------------------ DCTL register --------------------
 
@@ -70,7 +88,13 @@ static void usb_dwc_ll_dctl_set_soft_disconnect(usb_dwc_dev_t *hw, bool enable)
 static void vbus_appeared(void)
 {
     ESP_LOGD(TAG, "Appeared");
+#if (BVALID_OVERRIDE_SUPPORT_ENABLE)
     usb_dwc_ll_gotgctl_set_bvalid_override_value(&USB_DWC_REG, 1);
+#else
+    // Workaround for ESP32-P4 ECO5, USB-OTG peripheral v4.30a
+    // tud_mount_cb() will be called after SetConfiguration()
+    // So we do not need to notify the upper logic about the connection event manually
+#endif // BVALID_OVERRIDE_SUPPORT_ENABLE
     usb_dwc_ll_dctl_set_soft_disconnect(&USB_DWC_REG, false);
 }
 
@@ -80,8 +104,16 @@ static void vbus_appeared(void)
 static void vbus_disappeared(void)
 {
     ESP_LOGD(TAG, "Disappeared");
-    usb_dwc_ll_gotgctl_set_bvalid_override_value(&USB_DWC_REG, 0);
     usb_dwc_ll_dctl_set_soft_disconnect(&USB_DWC_REG, true);
+#if (BVALID_OVERRIDE_SUPPORT_ENABLE)
+    usb_dwc_ll_gotgctl_set_bvalid_override_value(&USB_DWC_REG, 0);
+#else
+    // Workaround for ESP32-P4 ECO5, USB-OTG peripheral v4.30a
+    // We are not able to detect the disconnection event from the PHY after VBUS goes low
+    // So we need to notify the upper logic about the disconnection event manually
+    assert(_vbus_ctx.state_changed_cb != NULL); // Sanity check
+    _vbus_ctx.state_changed_cb(false);
+#endif // BVALID_OVERRIDE_SUPPORT_ENABLE
 }
 
 /**
@@ -141,6 +173,7 @@ static void vbus_debounce_timer_cb(TimerHandle_t xTimer)
 esp_err_t tinyusb_vbus_monitor_init(tinyusb_vbus_monitor_config_t *config)
 {
     ESP_RETURN_ON_FALSE(config != NULL, ESP_ERR_INVALID_ARG, TAG, "Invalid argument: config is NULL");
+    ESP_RETURN_ON_FALSE(config->state_changed_cb != NULL, ESP_ERR_INVALID_ARG, TAG, "Invalid argument: state_changed_cb is NULL");
 
     esp_err_t ret;
     // There could be only one instance of VBUS monitoring
@@ -149,8 +182,17 @@ esp_err_t tinyusb_vbus_monitor_init(tinyusb_vbus_monitor_config_t *config)
         return ESP_ERR_INVALID_STATE;
     }
 
+#if (CONFIG_TINYUSB_MSC_ENABLED)
+    // When MSC is enabled, timer task stack size must be sufficient to handle FS operations during detach events
+#if (configTIMER_TASK_STACK_DEPTH < VBUS_MON_TIMER_TASK_STACK_MIN)
+    ESP_LOGE(TAG, "When MSC is enabled, configTIMER_TASK_STACK_DEPTH must be at least %d bytes to handle FS operations during attach/detach events", VBUS_MON_TIMER_TASK_STACK_MIN);
+    return ESP_ERR_NOT_SUPPORTED;
+#endif // (configTIMER_TASK_STACK_DEPTH)
+#endif // (CONFIG_TINYUSB_MSC_ENABLED)
+
     _vbus_ctx.gpio_num = config->gpio_num;
     _vbus_ctx.prev_state = false;
+    _vbus_ctx.state_changed_cb = config->state_changed_cb;
 
     // VBUS Debounce timer
     _vbus_ctx.debounce_timer = xTimerCreate("vbus_debounce_timer",
@@ -185,12 +227,15 @@ esp_err_t tinyusb_vbus_monitor_init(tinyusb_vbus_monitor_config_t *config)
     }
     // Disable GPIO interrupt
     gpio_intr_disable(_vbus_ctx.gpio_num);
+
+#if (BVALID_OVERRIDE_SUPPORT_ENABLE)
     // Set initial Bvalid override value and enable override
     usb_dwc_ll_gotgctl_set_bvalid_override_value(&USB_DWC_REG, 0);
     // Wait 1 microsecond (sufficient for >5 PHY clocks)
     esp_rom_delay_us(1);
     // Enable to override the signal from PHY
     usb_dwc_ll_gotgctl_enable_bvalid_override(&USB_DWC_REG, true);
+#endif // BVALID_OVERRIDE_SUPPORT_ENABLE
 
     // Device could be already connected, check the status and start the timer if needed
     if (gpio_get_level(_vbus_ctx.gpio_num)) {
@@ -210,7 +255,9 @@ esp_err_t tinyusb_vbus_monitor_init(tinyusb_vbus_monitor_config_t *config)
 
 timer_err:
     gpio_isr_handler_remove(_vbus_ctx.gpio_num);
+#if (BVALID_OVERRIDE_SUPPORT_ENABLE)
     usb_dwc_ll_gotgctl_enable_bvalid_override(&USB_DWC_REG, false);
+#endif // BVALID_OVERRIDE_SUPPORT_ENABLE
 isr_err:
     gpio_reset_pin(_vbus_ctx.gpio_num);
     _vbus_ctx.gpio_num = GPIO_NUM_NC;
@@ -244,8 +291,11 @@ esp_err_t tinyusb_vbus_monitor_deinit(void)
     xTimerDelete(_vbus_ctx.debounce_timer, 0);
     _vbus_ctx.debounce_timer = NULL;
 
+#if (BVALID_OVERRIDE_SUPPORT_ENABLE)
     // Disable to override the signal from PHY
     usb_dwc_ll_gotgctl_enable_bvalid_override(&USB_DWC_REG, false);
+#endif // BVALID_OVERRIDE_SUPPORT_ENABLE
+
     ESP_LOGD(TAG, "Deinit");
     return ESP_OK;
 }
