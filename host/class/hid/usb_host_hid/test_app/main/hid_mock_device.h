b47906578195f48daea498d862605706408b7a5c
Roman Leonov <roman.leonov@espressif.com>
2025-11-14 16:12:51 +0100

    feature(usb_host_hid): Added test case for the large report descriptor
    - Refactored the way how hid mock device is configured, introduced _test_hid_mock_device_mode param
    - Added the third hid mock device with large report descriptor (1905 bytes) to test the realloc of ctrl_xfer during usb_class_request_get_descriptor()
    - Added additional run in pytest with mocked hid device with large descriptor

--- host/class/hid/usb_host_hid/test_app/main/hid_mock_device.h
+++ host/class/hid/usb_host_hid/test_app/main/hid_mock_device.h
@@ -1,13 +1,18 @@
 /*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <stdint.h>
 
-typedef enum {
-    TUSB_IFACE_COUNT_ONE = 0x00,
-    TUSB_IFACE_COUNT_TWO = 0x01,
-    TUSB_IFACE_COUNT_MAX
-} tusb_iface_count_t;
+// Define test HID mock device types
+enum test_hid_mock_device {
+    TEST_HID_MOCK_DEVICE_WITH_ONE_IFACE = 0,
+    TEST_HID_MOCK_DEVICE_WITH_TWO_IFACES,
+    TEST_HID_MOCK_DEVICE_WITH_LARGE_REPORT,
+    TEST_HID_MOCK_DEVICE_MAX,
+};
 
-void hid_mock_device(tusb_iface_count_t iface_count);
+void hid_mock_device_set_mode(const enum test_hid_mock_device mode);
+
+void hid_mock_device_run(void);
