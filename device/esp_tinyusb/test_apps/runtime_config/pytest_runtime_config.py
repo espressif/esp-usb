e3b601de9be00798c1dea8c7d27dd11d988dbe63
Roman Leonov <roman.leonov@espressif.com>
2025-11-17 15:46:42 +0100

    feature(esp_tinyusb): Added CPU load test case in runtime_config test

--- device/esp_tinyusb/test_apps/runtime_config/pytest_runtime_config.py
+++ device/esp_tinyusb/test_apps/runtime_config/pytest_runtime_config.py
@@ -20,3 +20,39 @@ def test_usb_device_runtime_config(dut: IdfDut) -> None:
 
     for periph in peripherals:
         dut.run_all_single_board_cases(group=periph)
+
+# The threshold values for TinyUSB Task Run time (in cycles) for different targets
+TASK_RUN_TIME_LIMITS = {
+    'esp32s2': 5000,
+    'esp32s3': 3000,
+    'esp32p4': 1800,
+}
+
+def _get_run_time_th(target: str) -> int:
+    assert target in TASK_RUN_TIME_LIMITS
+    return TASK_RUN_TIME_LIMITS.get(target)
+
+@pytest.mark.esp32s2
+@pytest.mark.esp32s3
+@pytest.mark.esp32p4
+@pytest.mark.usb_device
+def test_cpu_load_task_stat_print(dut: IdfDut) -> None:
+    '''
+    Test to verify that Run time and CPU load measurement for TinyUSB task is working.
+    This test runs only on runtime_config test app.
+
+    Test procedure:
+    1. Run the test on the DUT
+    2. Expect to see TinyUSB task CPU load printed in the output
+    3. Expect TinyUSB task CPU load to be not greater than 0%
+    '''
+    dut.expect_exact('Press ENTER to see the list of tests.')
+    dut.write('[cpu_load]')
+    dut.expect_exact('Starting TinyUSB load measurement test...')
+    dut.expect_exact('CPU load measurement test completed.')
+
+    line = dut.expect(r'TinyUSB Run time: (\d+) ticks')
+    run_time = int(line.group(1))
+    run_time_max = _get_run_time_th(dut.target)
+
+    assert 0 < run_time < run_time_max, f'Unexpected TinyUSB Run time: {run_time}'
