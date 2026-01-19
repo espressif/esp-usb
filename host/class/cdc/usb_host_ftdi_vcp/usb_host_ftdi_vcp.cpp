56080cb421eb7497e2e9e30e1bd245bd4bbebba2
igor.masar <igor.masar@espressif.com>
2026-01-19 17:13:49 +0100

    fix(usb_host_ftdi_vcp): avoid bitfield truncation in ftdi_rx
    Convert SerialState bit extraction to boolean (!= 0) so 1-bit fields are set
    correctly, and guard status parsing with data_len >= 2.

--- host/class/cdc/usb_host_ftdi_vcp/usb_host_ftdi_vcp.cpp
+++ host/class/cdc/usb_host_ftdi_vcp/usb_host_ftdi_vcp.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -91,16 +91,16 @@ bool FT23x::ftdi_rx(const uint8_t *data, size_t data_len, void *user_arg)
     FT23x *this_ftdi = (FT23x *)user_arg;
 
     // Dispatch serial state if it has changed
-    if (this_ftdi->user_event_cb) {
+    if (this_ftdi->user_event_cb && data_len >= 2) {
         cdc_acm_uart_state_t new_state;
         new_state.val = 0;
-        new_state.bRxCarrier =  data[0] & 0x80; // DCD
-        new_state.bTxCarrier =  data[0] & 0x20; // DSR
-        new_state.bBreak =      data[1] & 0x10;
-        new_state.bRingSignal = data[0] & 0x40;
-        new_state.bFraming =    data[1] & 0x08;
-        new_state.bParity =     data[1] & 0x04;
-        new_state.bOverRun =    data[1] & 0x02;
+        new_state.bRxCarrier =  (data[0] & 0x80u) != 0; // DCD
+        new_state.bTxCarrier =  (data[0] & 0x20u) != 0; // DSR
+        new_state.bBreak =      (data[1] & 0x10u) != 0;
+        new_state.bRingSignal = (data[0] & 0x40u) != 0;
+        new_state.bFraming =    (data[1] & 0x08u) != 0;
+        new_state.bParity =     (data[1] & 0x04u) != 0;
+        new_state.bOverRun =    (data[1] & 0x02u) != 0;
 
         if (this_ftdi->uart_state != new_state.val) {
             cdc_acm_host_dev_event_data_t serial_event;
