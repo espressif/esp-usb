18451e0a60c2829dfee4bc651caca1c0066e0159
peter.marcisovsky <peter.marcisovsky@espressif.com>
2026-01-22 18:18:58 +0100

    feat(hid_host): Remote wakeup

--- host/class/hid/usb_host_hid/test_app/pytest_usb_host_hid.py
+++ host/class/hid/usb_host_hid/test_app/pytest_usb_host_hid.py
@@ -22,33 +22,24 @@ def test_usb_host_hid(dut: Tuple[IdfDut, IdfDut]) -> None:
     device = dut[0]
     host = dut[1]
 
-    # Tests with mocked HID device with one Interface for HID tests
-    device.expect_exact('Press ENTER to see the list of tests.')
-    device.write('[hid_device]')
-    device.expect_exact('HID mock device with 1xInterface (Protocol=None) has been started')
+    hid_dev = "HID mock device with"
+    tests = [
+        ("hid_device",                     ["hid_host", "hid_host_pm"],     f"{hid_dev} 1xInterface (Protocol=None) has been started"),
+        ("hid_device_two_ifaces",          ["hid_host", "hid_host_pm"],     f"{hid_dev} 2xInterfaces (Protocol=BootKeyboard, Protocol=BootMouse) has been started"),
+        ("hid_device_large_report",        ["hid_host"],                    f"{hid_dev} large report descriptor has been started"),
+        ("hid_device_extra_large_report",  ["hid_host_extra_large_report"], f"{hid_dev} extra large report descriptor has been started"),
+        ("hid_device_suspend_dconn",       ["hid_host_suspend_dconn"],      f"{hid_dev} 2xInterfaces (Protocol=BootKeyboard, Protocol=BootMouse) has been started"),
+        ("hid_device_resume_dconn",        ["hid_host_resume_dconn"],       f"{hid_dev} 2xInterfaces (Protocol=BootKeyboard, Protocol=BootMouse) has been started"),
+        ("hid_device_remote_wake",         ["hid_host_remote_wake"],        f"{hid_dev} 2xInterfaces (Protocol=BootKeyboard, Protocol=BootMouse) has been started"),
+    ]
 
-    host.run_all_single_board_cases(group='hid_host')
+    for dev_test_mode, host_test_case_groups, device_expect_string in tests:
+        # Prepare USB Device for test
+        device.expect_exact("Press ENTER to see the list of tests.")
+        device.write(f"[{dev_test_mode}]")
+        device.expect_exact({device_expect_string})
 
-    # Tests with mocked HID device with two Interfaces for HID tests
-    device.serial.hard_reset()
-    device.expect_exact('Press ENTER to see the list of tests.')
-    device.write('[hid_device2]')
-    device.expect_exact('HID mock device with 2xInterfaces (Protocol=BootKeyboard, Protocol=BootMouse) has been started')
-
-    host.run_all_single_board_cases(group='hid_host')
-
-    # Tests with mocked HID device with large report descriptor (1905 bytes) for HID tests
-    device.serial.hard_reset()
-    device.expect_exact('Press ENTER to see the list of tests.')
-    device.write('[hid_device_large_report]')
-    device.expect_exact('HID mock device with large report descriptor has been started')
-
-    host.run_all_single_board_cases(group='hid_host')
-
-    # Tests with mocked HID device with extra large report descriptor (32KB) for HID tests
-    device.serial.hard_reset()
-    device.expect_exact('Press ENTER to see the list of tests.')
-    device.write('[hid_device_extra_large_report]')
-    device.expect_exact('HID mock device with extra large report descriptor has been started')
-
-    host.run_all_single_board_cases(group='hid_host_extra_large_report')
+        # Run tests USB Host tests
+        for host_test_case_group in host_test_case_groups:
+            host.run_all_single_board_cases(group=host_test_case_group)
+        device.serial.hard_reset()
