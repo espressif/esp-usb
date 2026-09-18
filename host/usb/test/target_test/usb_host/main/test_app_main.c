91d1566995b8081dbd0e3817ab8782b151f6827a
Tomas Rezucha <tomas.rezucha@espressif.com>
2026-09-18 17:06:24 +0200

    wip: test with IRAM

--- host/usb/test/target_test/usb_host/main/test_app_main.c
+++ host/usb/test/target_test/usb_host/main/test_app_main.c
@@ -42,7 +42,7 @@ void setUp(void)
     usb_host_config_t host_config = {
         .skip_phy_setup = true,
         .root_port_unpowered = false,
-        .intr_flags = ESP_INTR_FLAG_LOWMED,
+        .intr_flags = ESP_INTR_FLAG_LOWMED | ESP_INTR_FLAG_IRAM,
         .peripheral_map = TEST_PERIPHERAL_MAP,
     };
     ESP_ERROR_CHECK(usb_host_install(&host_config));
